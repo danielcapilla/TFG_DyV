7bbc6a6a74fc311309ba8783317659a64c8eb6c4
VictorCabanillas <vector.uno4@gmail.com>
2024-03-28 22:47:42 +0100

    player rb

--- TFG/Assets/Scripts/Player/PlayerController.cs
+++ TFG/Assets/Scripts/Player/PlayerController.cs
@@ -3,14 +3,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] CharacterController cController;
+    [SerializeField] float force, rotationSpeed;
+    Rigidbody rb;
     PlayerInput playerInput;
     Vector2 input;
+
+    Vector3 forward;
+    Vector3 right;
+    Camera cam;
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
         playerInput.actions["Interact"].performed += Interact;
+
+        cam = Camera.main;
+
+        forward = cam.transform.forward;
+        right = cam.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
     }
 
     // Update is called once per frame
@@ -21,7 +38,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        cController.Move(new Vector3(input.x,0,input.y));
+        //Vector3 desiredMovement = (forward * input.y + right * input.x);
+
+        Vector3 desiredMovement = new Vector3(input.x, 0f, input.y) * force;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMovement), rotationSpeed);
+        //rb.AddForce(new Vector3(input.x, 0f, input.y)*force);
+        rb.AddForce(desiredMovement);
     }
 
     public void Interact(InputAction.CallbackContext context) 
