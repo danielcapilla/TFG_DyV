eaeb17adf60303f58101294b19be8142aeba5635
VictorCabanillas <vector.uno4@gmail.com>
2024-03-28 11:12:36 +0100

    Screencontrols hidding and prefab

---
+++ TFG/Assets/Scripts/UI/HideScreenControls.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HideScreenControls : MonoBehaviour
+{
+    [SerializeField] GameObject stick;
+    [SerializeField] GameObject interactButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SystemInfo.deviceType == DeviceType.Handheld || Application.isMobilePlatform) 
+        {
+            stick.SetActive(true);
+            interactButton.SetActive(true);
+        }
+    }
+
+}
