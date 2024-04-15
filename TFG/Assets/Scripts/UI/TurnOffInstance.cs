6cdae45c5668613b80719738d7b7e28ff56ca15b
VictorCabanillas <vector.uno4@gmail.com>
2024-04-15 18:07:02 +0200

    Update TurnOffInstance.cs

--- TFG/Assets/Scripts/UI/TurnOffInstance.cs
+++ TFG/Assets/Scripts/UI/TurnOffInstance.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class TurnOffInstance : MonoBehaviour
+public class TurnOffInstance : NetworkBehaviour
 {
     private MeshRenderer visual;
 
     private void Start()
     {
-        visual = GetComponent<MeshRenderer>();
-        if (!HostManager.activarEscena)
+        if(IsHost || IsServer) 
         {
-            visual.enabled = false;
+            visual = GetComponent<MeshRenderer>();
+            if (!HostManager.activarEscena)
+            {
+                visual.enabled = false;
+            }
         }
-
     }
 }
