c6fcb57a4954f5377063b4abfb1e0e12bfb0ec85
VictorCabanillas <vector.uno4@gmail.com>
2024-04-16 20:30:29 +0200

    Quick login and player data

---
+++ TFG/Assets/Scripts/UI/LoginBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LoginBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField input;
+    public void Login() 
+    {
+        if (input.text.Length > 0)
+        {
+            //Guardar info en la clase statica
+            PlayerData.Name = input.text;
+            Debug.Log("Pasando a main menu");
+            SceneManager.LoadScene("MainMenu");
+        }
+        else 
+        {
+            //Poner en rojo y avisar de que hay que introducir info en los campos
+        }
+    }
+}
