fa2d8d7d94c74a41dc1a7410bfb33201fb762c26
Daniel Capilla Sánchez <capilloski@gmail.com>
2024-04-17 17:27:42 +0200

    TarjetitaDespawn

--- TFG/Assets/Scripts/UI/LobbyManager.cs
+++ TFG/Assets/Scripts/UI/LobbyManager.cs
@@ -28,9 +28,23 @@ public class LobbyManager : NetworkBehaviour
         {
             joinCodeTMP.text = TestRelay.staticCode;
             NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
         }
         
     }
+
+    private void OnClientDisconnected(ulong id)
+    {
+        TarjetitaScript[] tarjetitaArray = GameObject.FindObjectsOfType<TarjetitaScript>();
+        foreach (TarjetitaScript tarjetita in tarjetitaArray)
+        {
+            if(tarjetita.GetComponent<NetworkObject>().OwnerClientId == id)
+            {
+                tarjetita.GetComponent<NetworkObject>().Despawn();
+            }
+        }
+    }
+
     private void OnClientConnected(ulong clientId)
     {
         ShowJoinCodeClientRPC();
@@ -46,7 +60,7 @@ public class LobbyManager : NetworkBehaviour
     {
         GameObject instance = Instantiate(tarjetitaPrefab);
         NetworkObject instanceNetworkObject = instance.GetComponent<NetworkObject>();
-        instanceNetworkObject.Spawn(true);
+        instanceNetworkObject.SpawnWithOwnership(id);
         instance.transform.SetParent(layout.transform, false);
         TarjetitaScript tarjetita = instance.GetComponent<TarjetitaScript>();
         UserNetworkConfig userNetwork = NetworkManager.Singleton.ConnectedClients[id].PlayerObject.gameObject.GetComponent<UserNetworkConfig>();
@@ -64,6 +78,7 @@ public class LobbyManager : NetworkBehaviour
         base.OnNetworkDespawn();
         if (!IsServer) return;
         NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnected;
     }
 
     public void IrAJuego()
