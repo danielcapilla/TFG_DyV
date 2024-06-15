63232f0a5d64f7304e2e4c3c6f00f4b26920f7cd
VictorCabanillas <vector.uno4@gmail.com>
2024-06-15 11:36:51 +0200

    AudioSettings

--- TFG/Assets/Scripts/SettingsMenu.cs
+++ TFG/Assets/Scripts/SettingsMenu.cs
@@ -1,23 +1,71 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+
+    [SerializeField] AudioMixer Mixer;
+
+    [SerializeField] Slider MasterVolumeSlider;
+    [SerializeField] TextMeshProUGUI MasterVolumeText;
+
+    [SerializeField] Slider MusicVolumeSlider;
+    [SerializeField] TextMeshProUGUI MusicVolumeText;
+
+    [SerializeField] Slider SFXVolumeSlider;
+    [SerializeField] TextMeshProUGUI SFXVolumeText;
+
     // Start is called before the first frame update
     void Start()
     {
-        ChangeLanguage(PlayerPrefs.GetInt("Language"));
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Initialize()
     {
+        ChangeLanguage(PlayerPrefs.GetInt("Language"));
+        MasterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        MusicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        SFXVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
 
+        ChangeMasterVolume();
+        ChangeMusicVolume();
+        ChangeSFXVolume();
     }
 
     public void ChangeLanguage(int language)
     {
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[language];
         PlayerPrefs.SetInt("Language", language);
+    }
+
+    public void ChangeMasterVolume()
+    {
+        float value = MasterVolumeSlider.value;
+        MasterVolumeText.text = (100f * value).ToString("0");
+        Mixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+
+        PlayerPrefs.SetFloat("MasterVolume", value);
+    }
+
+    public void ChangeMusicVolume()
+    {
+        float value = MusicVolumeSlider.value;
+        MusicVolumeText.text = (100f * value).ToString("0");
+        Mixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    public void ChangeSFXVolume()
+    {
+        float value = SFXVolumeSlider.value;
+        SFXVolumeText.text = (100f * value).ToString("0");
+        Mixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
+
+        PlayerPrefs.SetFloat("SFXVolume", value);
     }
 }
