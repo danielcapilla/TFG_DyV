8ca509818d3a8f0a04112de104d9caff576984fd
Daniel Capilla Sánchez <100380211+danielcapilla@users.noreply.github.com>
2025-10-13 12:20:55 +0200

    Update DataBaseCommander.cs

--- TFG/Assets/Scripts/DataBase/Connection/DataBaseCommander.cs
+++ TFG/Assets/Scripts/DataBase/Connection/DataBaseCommander.cs
@@ -401,8 +401,7 @@ public class DataBaseCommander : MonoBehaviour
     string CreatePostGameJSONChicken(string teacherCode, string classCode, string matchJSON)
     {
         DateTime dateTime = DateTime.Today;
-
-        //Construye JSON para la petici REST         
+       
         string json = $@"{{
             ""username"":""{Username}"",
             ""password"":""{Password}"",
